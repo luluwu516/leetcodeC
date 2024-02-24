708f5654eff99c0b85379567ab3e585374ab092b
Lulu Wu <sky110423@gmail.com>
2024-02-23 22:28:46 -0800

    Allocate memory

--- 14-LongestCommonPrefix/14-s2.c
+++ 14-LongestCommonPrefix/14-s2.c
@@ -5,17 +5,35 @@
 char *longestCommonPrefix(char **strs, int strsSize);
 
 int main() {
-  // Declaration
-  char *strs[] = {"flower", "flow", "flight"};
-  int size = sizeof(strs) / sizeof(strs[0]);
+  // declaration
+  int size;
+  char input[120];
+  char *prefix;
+
+  // input
+  printf("Enter the size of the list: ");
+  scanf("%d", &size);
+
+  char *strs[size];
+  char **strsPtr = strs;
+
+  printf("Enter a list of strings separated by new line: \n");
+  for (int i = 0; i < size; i++) {
+    printf("> ");
+    scanf("%s", input);
+    strs[i] = strdup(input);
+  }
 
   // Processing
-  char *prefix = longestCommonPrefix(strs, size);
+  prefix = longestCommonPrefix(strs, size);
 
   // Output
-  printf("The longest common prefix: %s\n", prefix);
+  printf("\nThe longest common prefix: %s\n", prefix);
 
   free(prefix);
+  for (int i = 0; i < size; i++) {
+    free(strs[i]);
+  }
 
   return 0;
 }
