ea3d130d274a837d86464f4e54b81c3162ff2d42
Lulu Wu <sky110423@gmail.com>
2024-05-17 20:48:40 -0700

    168. Excel Sheet Column Title

---
+++ 168-ExcelSheetColumnTitle/168-s1.c
@@ -0,0 +1,50 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+// function prototypes
+char* convertToTitle(int columnNumber);
+
+int main() {
+  // declaration
+  int columnNumber;
+  char* res;
+
+  // input
+  printf("\nEnter the intersect value: ");
+  scanf("%d", &columnNumber);
+
+  // processing
+  res = convertToTitle(columnNumber);
+
+  // output
+  printf("\n\nResult:\n");
+  printf("The corresponding column title is '%s'\n\n", res);
+
+  free(res);
+
+  return 0;
+}
+
+char* convertToTitle(int columnNumber) {
+  int len = 0, temp = columnNumber;
+  while (temp > 0) {
+    temp = (temp - 1) / 26;
+    len++;
+  }
+
+  char* res = (char*)malloc(sizeof(char) * (len + 1));
+  res[len] = '\0';
+
+  temp = columnNumber;
+  for (int i = len - 1; i >= 0; i--) {
+    if (temp % 26) {
+      res[i] = (temp % 26 - 1) + 'A';
+    } else {
+      res[i] = 'Z';
+      temp--;
+    }
+    temp /= 26;
+  }
+
+  return res;
+}
---
+++ 168-ExcelSheetColumnTitle/168-s2.c
@@ -0,0 +1,53 @@
+#include "stdio.h"
+#include "stdlib.h"
+
+// function prototypes
+char* convertToTitle(int columnNumber);
+
+int main() {
+  // declaration
+  int columnNumber;
+  char* res;
+
+  // input
+  printf("\nEnter the intersect value: ");
+  scanf("%d", &columnNumber);
+
+  // processing
+  res = convertToTitle(columnNumber);
+
+  // output
+  printf("\n\nResult:\n");
+  printf("The corresponding column title is '%s'\n\n", res);
+
+  free(res);
+
+  return 0;
+}
+
+char* convertToTitle(int columnNumber) {
+  int len = 0, temp = columnNumber;
+  char* res = (char*)calloc(10, sizeof(char));
+
+  while (temp != 0) {
+    if (temp % 26) {
+      *(res + len) = (temp % 26) + 'A' - 1;
+    } else {
+      *(res + len) = 'Z';
+      temp--;
+    }
+    len++;
+    temp /= 26;
+  }
+
+  int right = len - 1, left = 0;
+  while (left < right) {
+    char temp = res[left];
+    res[left] = res[right];
+    res[right] = temp;
+    left++;
+    right--;
+  }
+
+  return res;
+}
