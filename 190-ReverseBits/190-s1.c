409e0b915ec70c30f5613697e23da899f11920af
Lulu Wu <sky110423@gmail.com>
2024-05-24 16:19:34 -0700

    190. Reverse Bits

---
+++ 190-ReverseBits/190-s1.c
@@ -0,0 +1,42 @@
+#include "inttypes.h"
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+uint32_t reverseBits(uint32_t n);
+
+int main() {
+  // declaration
+  uint32_t num;
+  char input[33];
+  uint32_t res;
+
+  // input
+  printf("\nEnter the binary number: ");
+  scanf("%32s", input);
+
+  char *endptr;
+  num = strtoul(input, &endptr, 2);
+  printf("nums: %" PRIu32 "\n", num);
+
+  // processing
+  res = reverseBits(num);
+
+  // output
+  printf("\n\nResult:\n");
+  printf("The return number in decimal representation is '%d'\n\n", res);
+
+  return 0;
+}
+
+uint32_t reverseBits(uint32_t n) {
+  uint32_t res = 0;
+
+  for (int i = 0; i < 32; i++) {
+    res <<= 1;
+    res += n % 2;
+    n >>= 1;
+  }
+
+  return res;
+}
