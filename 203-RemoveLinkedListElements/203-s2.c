aefd4b007fb75d7f82556797063aba8f0756cf1d
Lulu Wu <sky110423@gmail.com>
2024-05-29 13:10:00 -0700

    203. Remove Linked List Elements

---
+++ 203-RemoveLinkedListElements/203-s2.c
@@ -0,0 +1,103 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX_SIZE 10000
+
+struct ListNode {
+  int val;
+  struct ListNode* next;
+};
+
+// function prototype
+struct ListNode* removeElements(struct ListNode* head, int val);
+struct ListNode* constructLinkList(int* nums, int size);
+void printLinkList(struct ListNode* head);
+void freeLinkList(struct ListNode* head);
+
+int main() {
+  // declaration
+  int num, i = 0;
+  int nums[MAX_SIZE];
+  struct ListNode *head, *res;
+
+  // input
+  printf("\nEnter numbers to build a Linked List, -1 to stop: \n");
+  while (1) {
+    printf("> ");
+    scanf("%d", &num);
+    if (num == -1) {
+      break;
+    }
+    nums[i++] = num;
+  }
+
+  printf("\nEnter a value to remove: ");
+  scanf("%d", &num);
+
+  head = constructLinkList(nums, i);
+
+  // processing and output
+  printf("\nResult:\n");
+  printf("Original: ");
+  printLinkList(head);
+
+  res = removeElements(head, num);
+  printf("Revised : ");
+  printLinkList(res);
+
+  freeLinkList(res);
+
+  return 0;
+}
+
+struct ListNode* constructLinkList(int* nums, int size) {
+  if (size == 0) return NULL;
+
+  struct ListNode* head = (struct ListNode*)malloc(sizeof(struct ListNode));
+  struct ListNode* curr = head;
+  curr->val = nums[0];
+
+  for (int i = 1; i < size; ++i) {
+    curr->next = (struct ListNode*)malloc(sizeof(struct ListNode));
+    curr = curr->next;
+    curr->val = nums[i];
+  }
+  curr->next = NULL;  // Mark the end of the list
+
+  return head;
+}
+
+void printLinkList(struct ListNode* head) {
+  struct ListNode* curr = head;
+  while (curr) {
+    printf("%d ", curr->val);
+    curr = curr->next;
+  }
+  printf("\n");
+}
+
+void freeLinkList(struct ListNode* head) {
+  struct ListNode* curr = head;
+  while (curr) {
+    struct ListNode* temp = curr;
+    curr = curr->next;
+    free(temp);
+  }
+}
+
+struct ListNode* removeElements(struct ListNode* head, int val) {
+  struct ListNode dummy;
+  dummy.next = head;
+  struct ListNode* curr = &dummy;
+
+  while (curr->next != NULL) {
+    if (curr->next->val == val) {
+      struct ListNode* temp = curr->next;
+      curr->next = curr->next->next;
+      free(temp);
+    } else {
+      curr = curr->next;
+    }
+  }
+
+  return dummy.next;
+}
